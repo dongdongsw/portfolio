fa5b8917f659620159cd3dafb5a0db015916650f
서동현 <fishman4535@gmail.com>
2025-07-26 16:00:18 +0900

    데이터베이스 생성 예시파일(User) 넣어놓음 그리고 데이터베이스 파일인 Entity파일을 만들기 위해서 	implementation 'org.springframework.boot:spring-boot-starter-data-jpa'
    의존성을 추가하였고
    application.properties에 데이터베이스 연결을 하는 코드들을 넣어 놓음 mysql에서 사용하는 username과 password를 넣으시면됨
    build.gradle을 한번 실행해주시고 demoApplication을 실행시키시면 서버가 멈추지 않고 잘 돌아갈 것입니다.

---
+++ src/main/java/com/example/demo/Login/Entity/User.java
@@ -0,0 +1,120 @@
+package com.example.demo.Login.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "user_info", uniqueConstraints = @UniqueConstraint(columnNames = {"externalId", "alias"}))
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String externalId;
+
+    private String phoneNumber;
+    private String email;
+    private String postcode;
+    private String address;
+    private String detailAddress;
+    private String extraAddress;
+    private String fullAddress;
+
+    @Column(nullable = false)
+    private String alias;
+
+
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getExternalId() {
+        return externalId;
+    }
+
+    public void setExternalId(String externalId) {
+        this.externalId = externalId;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDetailAddress() {
+        return detailAddress;
+    }
+
+    public void setDetailAddress(String detailAddress) {
+        this.detailAddress = detailAddress;
+    }
+
+    public String getExtraAddress() {
+        return extraAddress;
+    }
+
+    public void setExtraAddress(String extraAddress) {
+        this.extraAddress = extraAddress;
+    }
+
+    public String getFullAddress() {
+        return fullAddress;
+    }
+
+    public void setFullAddress(String fullAddress) {
+        this.fullAddress = fullAddress;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    // 전체 주소 가공
+    public void constructFullAddress() {
+        if (detailAddress != null && !detailAddress.isEmpty()) {
+            this.fullAddress = address + " " + detailAddress;
+        } else {
+            this.fullAddress = address;
+        }
+    }
+}
