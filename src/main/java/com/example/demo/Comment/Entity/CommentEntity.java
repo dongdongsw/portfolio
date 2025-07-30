a04e93ab6345c64aae0779eb19164d7068fec06d
dongdongsw <cjfdn0904@gmail.com>
2025-07-30 23:58:16 +0900

    철우: Entity 클래스 생성

--- src/main/java/com/example/demo/Comment/Entity/CommentEntity.java
+++ src/main/java/com/example/demo/Comment/Entity/CommentEntity.java
@@ -4,118 +4,33 @@ package com.example.demo.Comment.Entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.*;
 
+import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "user_info", uniqueConstraints = @UniqueConstraint(columnNames = {"externalId", "alias"}))
+@Table(name = "comment")
 public class CommentEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private int id;
 
-    @Column(nullable = false)
-    private String externalId;
+    @Column(name = "post_id", nullable = false)
+    private int postId;
 
-    private String phoneNumber;
-    private String email;
-    private String postcode;
-    private String address;
-    private String detailAddress;
-    private String extraAddress;
-    private String fullAddress;
-//--------------------------------------------------------------------------------
-    @Column(nullable = false)
-    private String alias;
+    @column(name = "login_id", length = 25, nullable = false)
+    private String loginId;
 
+    @column(name = "nickname", length = 20, nullable = false)
+    private String nickname;
 
+    @column(name = "content", length = 50)
+    private String content;
 
-    // Getters and Setters
-    public Long getId() {
-        return id;
-    }
+    @column(name = "upload_date")
+    private LocalDateTime uploadDate;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    @column(name = "modify_date")
+    private LocalDateTime modifyDate;
 
-    public String getExternalId() {
-        return externalId;
-    }
-
-    public void setExternalId(String externalId) {
-        this.externalId = externalId;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPostcode() {
-        return postcode;
-    }
-
-    public void setPostcode(String postcode) {
-        this.postcode = postcode;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getDetailAddress() {
-        return detailAddress;
-    }
-
-    public void setDetailAddress(String detailAddress) {
-        this.detailAddress = detailAddress;
-    }
-
-    public String getExtraAddress() {
-        return extraAddress;
-    }
-
-    public void setExtraAddress(String extraAddress) {
-        this.extraAddress = extraAddress;
-    }
-
-    public String getFullAddress() {
-        return fullAddress;
-    }
-
-    public void setFullAddress(String fullAddress) {
-        this.fullAddress = fullAddress;
-    }
-
-    public String getAlias() {
-        return alias;
-    }
-
-    public void setAlias(String alias) {
-        this.alias = alias;
-    }
-
-    // 전체 주소 가공
-    public void constructFullAddress() {
-        if (detailAddress != null && !detailAddress.isEmpty()) {
-            this.fullAddress = address + " " + detailAddress;
-        } else {
-            this.fullAddress = address;
-        }
-    }
-}
+    @column(name = "author")
+    private LocalDateTime author;
