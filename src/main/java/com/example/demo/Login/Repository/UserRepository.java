3642e819f6ead84021c066b470372041f8804681
dongdongsw <fishman4535@gmail.com>
2025-07-31 09:33:34 +0900

    <서동현> 마스터 브랜치에 있던 로그인 파일들을 로그인 브랜치로 이동 함(마스터 브랜치에 있는 로그인 파일 들은 삭제함

--- src/main/java/com/example/demo/Login/Repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Login.Repository;
-
-import com.example.demo.Login.Entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface UserRepository extends JpaRepository<User, Long>  {
-
-}
