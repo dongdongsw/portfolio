08c75bf8fd0a8b81ba4210695aed7019e2d39462
dongdongsw <fishman4535@gmail.com>
2025-08-12 13:35:33 +0900

    <서동현> 게시판, 댓글, 로그인 병합

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.example.demo.config;
+package com.example.demo.Config;
 
 
 import lombok.RequiredArgsConstructor;
@@ -25,24 +25,31 @@ public class SecurityConfig {
         http
                 .csrf().disable()   //api 개발할 때는 dusable로 해놓고 꺼놓고 실제 개발시에는 enable로 수정
                 .authorizeHttpRequests() //경로에 어떤 권한을 줄지 설정
-                    .requestMatchers(
+                .requestMatchers(
 //                            --------------------------로그인
-                            "/api/user/create",
-                            "/api/user/login",
-                            "/api/user/findid/send-auth",
-                            "/api/user/findid/verify-id",
-                            "/api/user/findpw/send-auth",
-                            "/api/user/findpw/verify-pw",
-                            "/api/user/logout",
-                            //---------------------------
-                            "/api/post",
-                            "/api/post/{id}",
-                            "/api/posts/{postId}/comment/view",
-                            "/api",
-                            "/css/**",
-                            "/js/**",
-                            "/images/**").permitAll() //이 경로는 누구나 접근 허용이 가능(로그인 하지 않은 사용자들도 인증없이)
-                    .anyRequest().authenticated() //그 외에는 모든 접근은 로그인된 사용자만 허락함
+                        "/api/user/create",
+                        "/api/user/login",
+                        "/api/user/findid/send-auth",
+                        "/api/user/findid/verify-id",
+                        "/api/user/findpw/send-auth",
+                        "/api/user/findpw/verify-pw",
+                        "/api/user/logout",
+                        //---------------------------
+                        "/api/post",
+                        "/api/post/{id}",
+                        "/api/posts/{postId}/comment/view",
+                        //---------------------------
+                        "/api/comments/commentscreate",
+                        "/api/comments/post/**",
+                        "/api/comments/singleview/**",
+                        "/api/comments/edit/**",
+                        "/api/comments/delete/**",
+                        //---------------------------
+                        "/api",
+                        "/css/**",
+                        "/js/**",
+                        "/images/**").permitAll() //이 경로는 누구나 접근 허용이 가능(로그인 하지 않은 사용자들도 인증없이)
+                .anyRequest().authenticated() //그 외에는 모든 접근은 로그인된 사용자만 허락함
 
                 .and() //authorizeHttpRequests()의 설정을 마무리하고 다음으로
 
@@ -51,8 +58,8 @@ public class SecurityConfig {
 //                    .loginProcessingUrl("/api/user/login") //로그인 처리하는 페이지
 //                    .defaultSuccessUrl("/api/") //로그인 성공하면 홈으로 이동
 //                    .failureUrl("/api/user/login?error=true") //로그인 실패시 이동되는 경로
-                    .httpBasic().disable()
-                    .formLogin().disable()
+                .httpBasic().disable()
+                .formLogin().disable()
 //                .and()//formLogi n()의 설정을 마무리하고 다음으로
                 .logout().disable();       // 선택적으로 로그아웃도 비활성화
 
@@ -65,10 +72,3 @@ public class SecurityConfig {
         return http.build(); //위의 설정들을 SpringSecurity가 사용할 수 있도록 반환
     }
 }
-
-
-
-
-
-
-
