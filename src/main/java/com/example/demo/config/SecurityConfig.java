a0524383aadf5104e999bdf5b55603e9820ce242
dongdongsw <cjfdn0904@gmail.com>
2025-09-08 21:17:48 +0900

    <이철우> 브랜치 수정 작업

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -33,38 +33,38 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())   //api 개발할 때는 dusable로 해놓고 꺼놓고 실제 개발시에는 enable로 수정
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(auth -> auth //경로에 어떤 권한을 줄지 설정
-                .requestMatchers(
-                        "/api/comments/post/**",
-                        "/api/comments/singleview/**",
-                        "/api/comments/edit/**",
-                        "/api/comments/delete/**"
-                        ).permitAll()
-                        .requestMatchers("/error").permitAll()
-                        .requestMatchers(
+                                .requestMatchers(
+                                        "/api/comments/post/**",
+                                        "/api/comments/singleview/**",
+                                        "/api/comments/edit/**",
+                                        "/api/comments/delete/**"
+                                ).permitAll()
+                                .requestMatchers("/error").permitAll()
+                                .requestMatchers(
 //                            --------------------------로그인
-                        "/api/user/create",
-                        "/api/user/login",
-                        "/api/user/findid/send-auth",
-                        "/api/user/findid/verify-id",
-                        "/api/user/findpw/send-auth",
-                        "/api/user/findpw/verify-pw",
-                        "/api/user/logout",
-                        //---------------------------
-                        "/api/mypage/**",
-                        //---------------------------
-                        "/api/posts",
-                        "/api/posts/detail/**",
-                        "/api/posts/modify/**",
-                        "/api/posts/delete/**",
-                        "/api/post/{id}",
-                        "/api/posts/{postId}/comment/view",
-                        //---------------------------
-                        "/api",
-                        "/css/**",
-                        "/js/**",
-                        "/images/**")
-                .permitAll() //이 경로는 누구나 접근 허용이 가능(로그인 하지 않은 사용자들도 인증없이)
-                .anyRequest().authenticated() //그 외에는 모든 접근은 로그인된 사용자만 허락함
+                                        "/api/user/create",
+                                        "/api/user/login",
+                                        "/api/user/findid/send-auth",
+                                        "/api/user/findid/verify-id",
+                                        "/api/user/findpw/send-auth",
+                                        "/api/user/findpw/verify-pw",
+                                        "/api/user/logout",
+                                        //---------------------------
+                                        "/api/mypage/**",
+                                        //---------------------------
+                                        "/api/posts",
+                                        "/api/posts/detail/**",
+                                        "/api/posts/modify/**",
+                                        "/api/posts/delete/**",
+                                        "/api/post/{id}",
+                                        "/api/posts/{postId}/comment/view",
+                                        //---------------------------
+                                        "/api",
+                                        "/css/**",
+                                        "/js/**",
+                                        "/images/**")
+                                .permitAll() //이 경로는 누구나 접근 허용이 가능(로그인 하지 않은 사용자들도 인증없이)
+                                .anyRequest().authenticated() //그 외에는 모든 접근은 로그인된 사용자만 허락함
                 )
 //                .formLogin()
 //                    .loginPage("/api/user/login") //로그인 입력하는 페이지
